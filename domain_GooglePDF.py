46b2a173e7b11e6238c48e3ec45434492f23318f
Nutan Kumar Panda <nkpanda4u.ever@gmail.com>
2016-06-17 13:52:29 +0530

    Update domain_GooglePDF.py
    the num parameter is used to get number of results

--- domain_GooglePDF.py
+++ domain_GooglePDF.py
@@ -12,7 +12,7 @@ This code is a bit messed up. Lists files from first page only. Needs a lot of m
 def googlesearch(query, ext):
 	print query
 	google="https://www.google.co.in/search?filter=0&q=site:" 
-	getrequrl="https://www.google.co.in/search?filter=0&q=%s&start=" % (query)
+	getrequrl="https://www.google.co.in/search?filter=0&num=100&q=%s&start=" % (query)
 	hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11', 
        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8', 
        'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3', 
