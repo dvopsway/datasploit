c92627770035e0b2a8f64d7acf923f32fbc4858b
upgoingstar <upgoingstaar@gmail.com>
2016-07-19 03:48:02 +0530

    Full Contact api integration

--- emailOsint.py
+++ emailOsint.py
@@ -7,6 +7,8 @@ import time
 import hashlib
 from bs4 import BeautifulSoup
 import re
+from email_fullcontact import fullcontact
+
 
 email = sys.argv[1]
 print email
@@ -72,7 +74,53 @@ for x in hbp:
 print "\n-----------------------------\n"
 '''
 print "\t\t\t[+] Finding user information based on emailId\n"
-clb_data = clearbit(email)
+
+data = fullcontact(email)
+if data.get("status","") == 200:
+	print "Name: %s" % data.get("contactInfo","").get('fullName', '')
+	print "\nOrganizations:"
+	for x in data.get("organizations",""):
+		if x.get('isPrimary', '') == True:
+			primarycheck = " - Primary"
+		else:
+			primarycheck = ""
+		if x.get('endDate','') == '':
+			print "\t%s at %s - (From %s to Unknown Date)%s" % (x.get('title', ''), x.get('name',''), x.get('startDate',''), primarycheck)
+		else:
+			print "\t%s - (From %s to %s)%s" % (x.get('name',''), x.get('startDate',''), x.get('endDate',''), primarycheck)
+	if data.get("contactInfo","").get('websites', '') != "":
+		print "\nWebsite(s):"
+		for x in data.get("contactInfo","").get('websites', ''):
+			print "\t%s" % x.get('url', '')
+	if data.get("contactInfo","").get('chats', '') != "":
+		print '\nChat Accounts'
+		for x in data.get("contactInfo","").get('chats', ''):
+			print "\t%s on %s" % (x.get('handle', ''), x.get('client', ''))
+	
+	print "\nSocial Profiles:"
+	for x in data.get("socialProfiles",""):
+		print "\t%s:" % x.get('type','').upper()
+		for y in x.keys():
+			if y != 'type' and y != 'typeName' and y != 'typeId':
+				print '\t%s: %s' % (y, x.get(y,''))
+		print ''
+
+	print "Other Details:"
+	if data.get("demographics","") != "":
+		print "\tGender: %s" % data.get("demographics","").get('gender', '')
+		print "\tCountry: %s" % data.get("demographics","").get('country', '')
+		print "\tTentative City: %s" % data.get("demographics","").get('locationGeneral', '')
+
+	print "Photos:"
+	for x in data.get("photos",""):
+		print "\t%s: %s" % (x.get('typeName', ''), x.get('url', ''))
+
+else:
+	print 'Error Occured - Encountered Status Code: %s' % data.get("status","")
+
+
+
+'''clb_data = clearbit(email)
 for x in clb_data.keys():
 	print '%s details:' % x
 	if type(clb_data[x]) == dict:
@@ -81,6 +129,7 @@ for x in clb_data.keys():
 				print "%s:  %s, " % (y, clb_data[x][y])
 	elif clb_data[x] is not None:
 		print "\n%s:  %s" % (x, clb_data[x])
+
 print "\n-----------------------------\n"
 
 print "\t\t\t[+] Gravatar Link\n"
@@ -90,6 +139,7 @@ print "\n-----------------------------\n"
 print "\t\t\t[+] Associated Domains\n"
 for doms in emaildom(email):
 	print doms
+'''
 print "\n-----------------------------\n"
 
 print "\t\t\t[+] Associated Slides\n"
@@ -102,8 +152,9 @@ print "\t\t\t[+] Associated Scribd Docs\n"
 scdlinks=emailscribddocs(email)
 for sl in scdlinks:
 	print sl
-	print "\n"
-print "\n"
+print ""
 print "More results might be available:"
 print "https://www.scribd.com/search?page=1&content_type=documents&query="+email
 print "\n-----------------------------\n"
+
+
---
+++ email_fullcontact.py
@@ -0,0 +1,63 @@
+import requests
+import sys
+import config as cfg
+import clearbit
+import json
+import time
+import hashlib
+from bs4 import BeautifulSoup
+import re
+
+
+
+def fullcontact(email):
+	req = requests.get("https://api.fullcontact.com/v2/person.json?email=%s&apiKey=%s" % (email, cfg.fullcontact_api))
+	data = json.loads(req.content)
+	return data
+
+
+email_id = sys.argv[1]
+data = fullcontact(email_id)
+if data.get("status","") == 200:
+	print "Name: %s" % data.get("contactInfo","").get('fullName', '')
+	print "\nOrganizations:"
+	for x in data.get("organizations",""):
+		if x.get('isPrimary', '') == True:
+			primarycheck = " - Primary"
+		else:
+			primarycheck = ""
+		if x.get('endDate','') == '':
+			print "\t%s at %s - (From %s to Unknown Date)%s" % (x.get('title', ''), x.get('name',''), x.get('startDate',''), primarycheck)
+		else:
+			print "\t%s - (From %s to %s)%s" % (x.get('name',''), x.get('startDate',''), x.get('endDate',''), primarycheck)
+	if data.get("contactInfo","").get('websites', '') != "":
+		print "\nWebsite(s):"
+		for x in data.get("contactInfo","").get('websites', ''):
+			print "\t%s" % x.get('url', '')
+	if data.get("contactInfo","").get('chats', '') != "":
+		print '\nChat Accounts'
+		for x in data.get("contactInfo","").get('chats', ''):
+			print "\t%s on %s" % (x.get('handle', ''), x.get('client', ''))
+	
+	print "\nSocial Profiles:"
+	for x in data.get("socialProfiles",""):
+		print "\t%s:" % x.get('type','').upper()
+		for y in x.keys():
+			if y != 'type' and y != 'typeName' and y != 'typeId':
+				print '\t%s: %s' % (y, x.get(y,''))
+		print ''
+
+	print "Other Details:"
+	if data.get("demographics","") != "":
+		print "\tGender: %s" % data.get("demographics","").get('gender', '')
+		print "\tCountry: %s" % data.get("demographics","").get('country', '')
+		print "\tTentative City: %s" % data.get("demographics","").get('locationGeneral', '')
+
+	print "Photos:"
+	for x in data.get("photos",""):
+		print "\t%s: %s" % (x.get('typeName', ''), x.get('url', ''))
+
+
+else:
+	print 'Error Occured - Encountered Status Code: %s' % data.get("status","")
+
