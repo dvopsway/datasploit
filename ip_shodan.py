70d3e7750f7cef5f7658b79da8029bedc75a3d61
upgoingstar <upgoingstaar@gmail.com>
2017-01-15 14:38:06 +0530

    error handling for non existing ip

--- ip_shodan.py
+++ ip_shodan.py
@@ -17,37 +17,39 @@ def shodansearch(ip):
 	endpoint =  "https://api.shodan.io/shodan/host/" + str(ip) + "?key=" + cfg.shodan_api
 	req = requests.get(endpoint)
 	parsed_res = json.loads(req.content)
-	print colored(style.BOLD + 'Report for IP: %s' + style.END, 'blue') % str(ip)
-	print colored(style.BOLD + '\n----------- Per Port Results -----------' + style.END)
-	if 'data' in parsed_res.keys():
-		for x in parsed_res['data']:
-			print colored(style.BOLD + '\nResponse from Open Port: %s'  + style.END, 'green') % (x['port'])
-			'''if 'title' in x.keys():
-				print colored(style.BOLD + '[+] Title:\t\t'  + style.END, 'green') + str(x['title'])'''
-			if 'title' in x.keys():	
-				print colored(style.BOLD + '[+] HTML Content:\t' + style.END, 'green') + str('Yes (Please inspect Manually on this port)')
-			if 'http' in x.keys():	
-				print colored(style.BOLD + '[+] HTTP port present:\t'  + style.END, 'green')
-				print '\tTitle: %s' % x['http']['title']
-				print '\tRobots: %s' % x['http']['robots']
-				print '\tServer: %s' % x['http']['server']
-				print '\tComponents: %s' % x['http']['components']
-				print '\tSitemap: %s' % x['http']['sitemap']
-			if 'ssl' in x.keys():
-				print '\tSSL Versions: %s' % x['ssl']['versions'] 	
-			print x['org']
-			asn = parsed_res['asn']
-			if 'vulns' in x['opts']:
-				for y in x['opts'].keys():
-					print x['opts'][y]
-	print colored(style.BOLD + '\n----------- Basic Info -----------' + style.END, 'blue')
-	print 'Open Ports: %s' % parsed_res['ports'] 
-	print 'Latitude: %s' % parsed_res['latitude']
-	print 'Hostnames: %s' % parsed_res['hostnames']
-	print 'Postal Code: %s' % parsed_res['postal_code']
-	print 'Country Code: %s' % parsed_res['country_code']
-	print 'ASN: %s' % asn
-	return req.content
+	if 'error' in parsed_res.keys():
+		print 'No information available for that IP.'
+	else:
+		print colored(style.BOLD + 'Report for IP: %s' + style.END, 'blue') % str(ip)
+		print colored(style.BOLD + '\n----------- Per Port Results -----------' + style.END)
+		if 'data' in parsed_res.keys():
+			for x in parsed_res['data']:
+				print colored(style.BOLD + '\nResponse from Open Port: %s'  + style.END, 'green') % (x['port'])
+				'''if 'title' in x.keys():
+					print colored(style.BOLD + '[+] Title:\t\t'  + style.END, 'green') + str(x['title'])'''
+				if 'title' in x.keys():	
+					print colored(style.BOLD + '[+] HTML Content:\t' + style.END, 'green') + str('Yes (Please inspect Manually on this port)')
+				if 'http' in x.keys():	
+					print colored(style.BOLD + '[+] HTTP port present:\t'  + style.END, 'green')
+					print '\tTitle: %s' % x['http']['title']
+					print '\tRobots: %s' % x['http']['robots']
+					print '\tServer: %s' % x['http']['server']
+					print '\tComponents: %s' % x['http']['components']
+					print '\tSitemap: %s' % x['http']['sitemap']
+				if 'ssl' in x.keys():
+					print '\tSSL Versions: %s' % x['ssl']['versions'] 	
+				print x['org']
+				asn = parsed_res['asn']
+				if 'vulns' in x['opts']:
+					for y in x['opts'].keys():
+						print x['opts'][y]
+		print colored(style.BOLD + '\n----------- Basic Info -----------' + style.END, 'blue')
+		print 'Open Ports: %s' % parsed_res['ports'] 
+		print 'Latitude: %s' % parsed_res['latitude']
+		print 'Hostnames: %s' % parsed_res['hostnames']
+		print 'Postal Code: %s' % parsed_res['postal_code']
+		print 'Country Code: %s' % parsed_res['country_code']
+		print 'ASN: %s' % asn
 
 def domaintoip(domain):
 	return socket.gethostbyname(domain)
