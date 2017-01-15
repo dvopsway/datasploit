140b73b1023834f5af48f36f521b729464ad401b
upgoingstar <upgoingstaar@gmail.com>
2017-01-15 14:35:19 +0530

    Ip Shodan Parsed Information

--- ip_shodan.py
+++ ip_shodan.py
@@ -5,11 +5,48 @@ import requests
 import json
 import sys
 import socket
+from termcolor import colored
+class style:
+   BOLD = '\033[1m'
+   END = '\033[0m'
 
+
+asn = ''
 def shodansearch(ip):
-	print "\t\t\t[+] Searching in Shodan" 
+	print colored(style.BOLD + '[+] Searching in Shodan' + style.END)
 	endpoint =  "https://api.shodan.io/shodan/host/" + str(ip) + "?key=" + cfg.shodan_api
 	req = requests.get(endpoint)
+	parsed_res = json.loads(req.content)
+	print colored(style.BOLD + 'Report for IP: %s' + style.END, 'blue') % str(ip)
+	print colored(style.BOLD + '\n----------- Per Port Results -----------' + style.END)
+	if 'data' in parsed_res.keys():
+		for x in parsed_res['data']:
+			print colored(style.BOLD + '\nResponse from Open Port: %s'  + style.END, 'green') % (x['port'])
+			'''if 'title' in x.keys():
+				print colored(style.BOLD + '[+] Title:\t\t'  + style.END, 'green') + str(x['title'])'''
+			if 'title' in x.keys():	
+				print colored(style.BOLD + '[+] HTML Content:\t' + style.END, 'green') + str('Yes (Please inspect Manually on this port)')
+			if 'http' in x.keys():	
+				print colored(style.BOLD + '[+] HTTP port present:\t'  + style.END, 'green')
+				print '\tTitle: %s' % x['http']['title']
+				print '\tRobots: %s' % x['http']['robots']
+				print '\tServer: %s' % x['http']['server']
+				print '\tComponents: %s' % x['http']['components']
+				print '\tSitemap: %s' % x['http']['sitemap']
+			if 'ssl' in x.keys():
+				print '\tSSL Versions: %s' % x['ssl']['versions'] 	
+			print x['org']
+			asn = parsed_res['asn']
+			if 'vulns' in x['opts']:
+				for y in x['opts'].keys():
+					print x['opts'][y]
+	print colored(style.BOLD + '\n----------- Basic Info -----------' + style.END, 'blue')
+	print 'Open Ports: %s' % parsed_res['ports'] 
+	print 'Latitude: %s' % parsed_res['latitude']
+	print 'Hostnames: %s' % parsed_res['hostnames']
+	print 'Postal Code: %s' % parsed_res['postal_code']
+	print 'Country Code: %s' % parsed_res['country_code']
+	print 'ASN: %s' % asn
 	return req.content
 
 def domaintoip(domain):
@@ -17,10 +54,9 @@ def domaintoip(domain):
 
 def main():
 	ip_addr = sys.argv[1]
-	print ip_addr
-	res_from_shodan = json.loads(shodansearch(ip_addr))
-	print res_from_shodan
-	print "\n-----------------------------\n"
+	shodansearch(ip_addr)
+	#print res_from_shodan
+	print colored(style.BOLD + '-----------------------------------' + style.END, 'blue')
 
 if __name__ == "__main__":
 	main()
