653a97cd2d276a980425c80e6b3859ea00ad8717
Anant Shrivastava <anant@anantshri.info>
2017-10-18 18:36:48 +0530

    fixing the master script detection logic

--- datasploit.py
+++ datasploit.py
@@ -2,54 +2,71 @@
 
 import re
 import sys
-import optparse
+import textwrap
+import argparse
 import emailOsint
 import domainOsint
 import ipOsint
 import usernameOsint
-
-parser = optparse.OptionParser()
-parser.add_option('-a', '--active', action="store", dest="domain", help="Launches Active Scans (work in progress)",
-                  default="spam")
-parser.add_option('-o', '--output', action="store", dest="output", help="Save output in either JSON or HTML")
-options, args = parser.parse_args()
-print options, args
-
-def printart():
-    print "\t                                                           "
-    print "\t   ____/ /____ _ / /_ ____ _ _____ ____   / /____   (_)/ /_"
-    print "\t  / __  // __ `// __// __ `// ___// __ \ / // __ \ / // __/"
-    print "\t / /_/ // /_/ // /_ / /_/ /(__  )/ /_/ // // /_/ // // /_  "
-    print "\t \__,_/ \__,_/ \__/ \__,_//____// .___//_/ \____//_/ \__/  "
-    print "\t                               /_/                         "
-    print "\t                                                           "
-    print "\t            Open Source Assistant for #OSINT               "
-    print "\t              Website: www.datasploit.info                 "
-    print "\t                                                           "
+from tld import get_tld
+from netaddr import IPAddress,AddrFormatError
 
 
-def main(user_input, output = None):
-    printart()
-    print "User Input: %s" % user_input
+def main(argv):
+    output=None
+    desc="""                                                           
+   ____/ /____ _ / /_ ____ _ _____ ____   / /____   (_)/ /_
+  / __  // __ `// __// __ `// ___// __ \ / // __ \ / // __/
+ / /_/ // /_/ // /_ / /_/ /(__  )/ /_/ // // /_/ // // /_  
+ \__,_/ \__,_/ \__/ \__,_//____// .___//_/ \____//_/ \__/  
+                               /_/                         
+                                                           
+            Open Source Assistant for #OSINT               
+                www.datasploit.info                                                           
 
-    if re.match('[^@]+@[^@]+\.[^@]+', user_input):
-        print "Looks like an EMAIL, running emailOsint...\n"
-        emailOsint.run(user_input, output)
-    elif re.match('^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$', user_input):
-        print "Looks like an IP, running ipOsint...\n"
-        ipOsint.run(user_input, output)
-    elif re.match('^[a-zA-Z\d-]{,63}(\.[a-zA-Z\d-]{,63}).$', user_input):
-        print "Looks like a DOMAIN, running domainOsint...\n"
-        domainOsint.run(user_input, output)
-    else:
-        print "Looks like a Username, running usernameOsint...\n"
-        usernameOsint.run(user_input, output)
-
+    """
+    epilog="""              Connect at Social Media: @datasploit                  
+                """
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,description=textwrap.dedent(desc),epilog=epilog)
+    parser.add_argument("-i","--input",help="Provide Input",dest='target',required=True)
+    parser.add_argument("-a","--active",help="Run Active Scan attacks",dest='active',action="store_false")
+    parser.add_argument("-q","--quiet",help="Run scans in automated manner accepting default answers",dest='quiet',action="store_false")
+    parser.add_argument("-o","--output",help="Provide Destination Directory",dest='output')
+    if len(argv) == 0:
+        parser.print_help()
+        sys.exit()
+    x=parser.parse_args()
+    active=x.active
+    quiet=x.quiet
+    user_input=x.target
+    output=x.output
+    print textwrap.dedent(desc)
+    try:    
+        print "User Input: %s" % user_input
+        try:
+            inp=IPAddress(user_input);
+            if inp.is_private() or inp.is_loopback():
+                print "Internal IP Detected : Skipping"
+                sys.exit()
+            else:
+                print "Looks like an IP, running ipOsint...\n"
+                ipOsint.run(user_input, output)
+        except SystemExit:
+            print "exiting"
+        except AddrFormatError:
+            if re.match('[^@]+@[^@]+\.[^@]+', user_input):
+                print "Looks like an EMAIL, running emailOsint...\n"
+                emailOsint.run(user_input, output)
+            elif get_tld(user_input, fix_protocol=True,fail_silently=True) is not None:
+                print "Looks like a DOMAIN, running domainOsint...\n"
+                domainOsint.run(user_input, output)
+            else:
+                print "Nothing Matched assuming username, running usernameOsint...\n"
+                usernameOsint.run(user_input, output)
+        except:
+            print "Unknown Error Occured"
+    except KeyboardInterrupt:
+        print "Ctrl+C called Quiting"
 
 if __name__ == "__main__":
-    try:
-        user_input = args[0]
-    except:
-        print "\n[-] Invalid Input. Exiting now..\n"
-        sys.exit(0)
-    main(user_input, options.output)
+   main(sys.argv[1:])
