ef02fd0171cbdc1ba7769d4a27e07a7386fd2cac
upgoingstar <upgoingstaar@gmail.com>
2018-01-04 01:38:29 +0530

    Added Email Search on Darknet

---
+++ emails/email_hacked_emails.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+import base
+import vault
+import sys
+import requests
+from termcolor import colored
+import json
+
+class style:
+    BOLD = '\033[1m'
+    END = '\033[0m'
+
+# Control whether the module is enabled or not
+ENABLED = True
+
+
+def banner():
+     print colored(style.BOLD + '\n---> Searching Email in DarkNet\n' + style.END, 'blue')
+
+
+def main(email):
+    req = requests.get("https://hacked-emails.com/api?q=%s" % email)
+    data = json.loads(req.text.encode('UTF-8'))
+    return data
+
+
+
+def output(data, email=""):
+    # Use the data variable to print out to console as you like
+    if data.get('status') == 'found':
+        print "%s Results found" % data.get('results')
+        for rec in data.get('data'):
+            print "------"
+            print colored(style.BOLD + 'Leak Title: ' + style.END) + str(rec.get('title'))
+            print colored(style.BOLD + 'Details: ' + style.END) + str(rec.get('details'))
+            if rec.get('source_url') == "#":
+                rec['source_url'] = "N/A"
+            print colored(style.BOLD + 'Leak URL: ' + style.END) + str(rec.get('source_url'))
+            print colored(style.BOLD + 'Leaked on: ' + style.END) + str(rec.get('date_created'))
+            if rec.get('source_provider') == 'anon':
+                rec['source_provider'] = "Anonymous"
+            print colored(style.BOLD + 'Source: ' + style.END) + str(rec.get('source_provider'))
+    else:
+        print "[-] No Data Found"
+
+
+
+
+if __name__ == "__main__":
+    try:
+        email = sys.argv[1]
+        banner()
+        result = main(email)
+        output(result, email)
+    except Exception as e:
+        print e
+        print "Please provide an email as argument"
